244108ed10b31249a9cb8344fcdce8175fd0f4ae
nadia zitout <nadiazitout@yahoo.fr>
2022-07-20 09:55:31 +0100

    fc d_d_n

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Model.player import Player
 from Model.round import Round
 import random
+import re
 from rich.console import Console
 from rich.table import Table
 
@@ -8,30 +9,40 @@ all_players = {}
 
 #def display_players_tournament():
 
+def int_date_birthday():
+
+    regex = re.compile(r'^[0-9]{2}/[0-9]{2}/[0-9]{4}$')
+    result = ""
+
+    result = input("votre date de naissance jj/mm/aaaa: ")
+    
+    while not re.findall(regex, result) :
+        result = input("Veuillez entrer une date de naissance valide: ")
+    
+    return result
+
 
 def create_players():
 
     for i in range(1, 3):
         p_name = input("ton nom? ")
         p_first_name = input("ton prénom? ")
-        # 3 input
-        p_d_o_b = int(input("Date de naissance? ")) 
+        p_d_o_b = int_date_birthday() 
         p_sex = input("Homme ou Femme? ")
         p = Player(p_name,p_first_name,p_d_o_b,p_sex)
         all_players['joueur_' + str(i)] = p
 
 create_players()
 
-print(all_players['joueur_2'])
-
 first_round = Round(name="premier_round",all_players=all_players)
-print(first_round.all_players)
 
+print(all_players['joueur_2'])
+print(first_round.all_players)
 first_round.display_match()
 print(first_round.filtered_players)
 #creer une instance de tournoi (joueur en liste (players))instance.methode
-
 print(first_round.all_players_keys)
+
 
 console = Console()
 table = Table(show_header=True, header_style="bold magenta")
